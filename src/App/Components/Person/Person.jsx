c5dafed7549747a985a3667dc192c51388a502e1
Prabin Upreti <upretirajprabin@gmail.com>
2020-10-05 17:24:19 +0545

    Added Form in person

--- src/App/Components/Person/Person.jsx
+++ src/App/Components/Person/Person.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Person() {
-  return <div>Person</div>;
+  return (
+    <>
+      <form action="" style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="Name">Name</label>
+        <input type="text" placeholder="Enter Name" />
+
+        <label htmlFor="Address">Address</label>
+        <input type="text" placeholder="Enter Address" />
+
+        <label htmlFor="Phone">Phone</label>
+        <input type="text" placeholder="Enter Phone" />
+
+        <label htmlFor="Description">Description</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default Person;
