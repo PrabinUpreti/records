9e2640278985afae84ed42fe105e640d9b9cdce7
Prabin Upreti <upretirajprabin@gmail.com>
2020-11-19 13:22:40 +0545

    Added Registration form and styled

--- src/App/Auth/Login.jsx
+++ src/App/Auth/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react';
 import "./Auth.css"
 import { authCheck, SignUp, SignIn } from './Auth'
+import LoginComponent from "./LoginComponent"
+import SignupComponent from "./SignupComponent"
 
 import {
     BrowserRouter as Router,
@@ -9,7 +11,8 @@ import {
     Route,
     Redirect,
     useHistory,
-    useLocation
+    useLocation,
+    Link
 } from "react-router-dom";
 import { useEffect } from 'react';
 
@@ -19,9 +22,16 @@ export default function Login() {
     let location = useLocation();
 
     const [credentials, setCredentials] = useState({
+
         username: '',
         password: ''
     })
+    const [signupCredentials, setSignupCredentials] = useState({
+        username: '',
+        password: '',
+        reEnteredPassword: ''
+    })
+    const [chooseForm, setChooseForm] = useState('login')
     const submitForm = (event) => {
         event.preventDefault();
         let u = credentials.username;
@@ -29,6 +39,17 @@ export default function Login() {
         SignIn(u, p, (signedUP) => {
             console.log(signedUP);
         })
+    }
+    const submitSignupForm = (e) => {
+
+        e.preventDefault();
+        let u = signupCredentials.username;
+        let p = signupCredentials.password;
+        console.log(signupCredentials);
+        SignUp(u, p, (signedUP) => {
+            console.log(signedUP);
+        })
+        setChooseForm("login")
     }
 
     useEffect(() => {
@@ -43,21 +64,35 @@ export default function Login() {
         })
     })
     return (
-        <div className="authForm">
-            <div className="logInForm">
-                <div className="leftContent">
-                    {/* <img src={'https://cdn.pixabay.com/photo/2017/04/19/00/59/office-2240932_960_720.png'} /> */}
-                </div>
-                <div className="rightContent">
-                    <form >
-                        <label htmlFor="">Name</label>
-                        <input value={credentials.username} onChange={(event) => { setCredentials({ ...credentials, username: event.target.value }) }} type="email" name="" id="" />
-                        <label htmlFor="">Password</label>
-                        <input value={credentials.password} onChange={(event) => { setCredentials({ ...credentials, password: event.target.value }) }} type="password" name="" id="" />
-                        <button className="authBtn" onClick={submitForm} type="submit">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+        // <div className="authForm">
+        //     <div className="logInForm">
+        //         <div className="leftContent">
+        //             {/* <img src={'https://cdn.pixabay.com/photo/2017/04/19/00/59/office-2240932_960_720.png'} /> */}
+        //         </div>
+        //         <div className="rightContent">
+        //             <form >
+        //                 <label htmlFor="">Name</label>
+        //                 <input value={credentials.username} onChange={(event) => { setCredentials({ ...credentials, username: event.target.value }) }} type="email" name="" id="" />
+        //                 <label htmlFor="">Password</label>
+        //                 <input value={credentials.password} onChange={(event) => { setCredentials({ ...credentials, password: event.target.value }) }} type="password" name="" id="" />
+        //                 <button className="authBtn" onClick={submitForm} type="submit">Submit</button>
+        //             </form>
+        //             <div className="register">
+        //                 <p>Don't have account? <Router><Link to="/signup">Register</Link> </Router> </p>
+        //                 <p>Forget password?<Router><Link to="#">Recover</Link></Router> </p>
+        //             </div>
+        //             <Switch>
+        //                 <Route path="/signup">
+        //                     <Signup></Signup>
+        //                 </Route>
+        //             </Switch>
+        //         </div>
+        //     </div>
+        // </div >
+
+        chooseForm == 'login' ?
+            <LoginComponent setChooseForm={setChooseForm} credentials={credentials} setCredentials={setCredentials} submitForm={submitForm}></LoginComponent> :
+            <SignupComponent setChooseForm={setChooseForm} credentials={signupCredentials} setCredentials={setSignupCredentials} submitForm={submitSignupForm}></SignupComponent>
+
     )
 }
---
+++ src/App/Auth/SignupComponent.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import "./Auth.css"
+export default function SignupComponent(props) {
+    return (
+        <div className="authForm">
+            <div className="logInForm">
+                <div className="leftContent">
+                    {/* <img src={'https://cdn.pixabay.com/photo/2017/04/19/00/59/office-2240932_960_720.png'} /> */}
+                </div>
+                <div className="rightContent">
+                    <form >
+                        <label htmlFor="">Name</label>
+                        <input value={props.credentials.username} onChange={(event) => { props.setCredentials({ ...props.credentials, username: event.target.value }) }} type="email" name="" id="" />
+                        <label htmlFor="">Password</label>
+                        <input value={props.credentials.password} onChange={(event) => { props.setCredentials({ ...props.credentials, password: event.target.value }) }} type="password" name="" id="" />
+                        <label htmlFor="">Re-Enter Password</label>
+                        <input style={{ border: props.credentials.password != props.credentials.reEnteredPassword ? "2px solid red" : "2px solid green" }} value={props.credentials.reEnteredPassword} onChange={(event) => { props.setCredentials({ ...props.credentials, reEnteredPassword: event.target.value }) }} type="password" name="" id="" />
+                        <button className="authBtn" onClick={props.submitForm} type="submit">Submit</button>
+                    </form>
+                    <div className="register">
+                        <p>Already have Account? <a onClick={() => props.setChooseForm("login")}>Login</a> </p>
+                        {/* <p>Forget password?<a>Recover</a> </p> */}
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+}
